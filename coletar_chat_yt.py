9374cd8fc8e7614553b491bc0031a79f596fe876
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2025-05-24 00:23:43 -0300

    Adicionar scripts para coletar mensagens de chat e metadados de vídeos do YouTube

--- coletar_chat_yt.py
+++ coletar_chat_yt.py
@@ -1,6 +1,6 @@
 from chat_downloader import ChatDownloader
 
-url = 'https://www.youtube.com/watch?v=gwmtr7_qqck'
+url = 'https://www.youtube.com/watch?v=9-ad9G6kxDM'
 chat = ChatDownloader().get_chat(url)
 
 for message in chat:
